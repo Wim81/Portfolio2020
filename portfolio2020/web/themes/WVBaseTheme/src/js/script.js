ac76b66a98e5732cd428f36756d8c6657b70da88
Wim Verpoorten <wim.verpoorten@hotmail.com>
2020-01-27 20:09:55 +0100

    title fix improvement: work node done

--- portfolio2020/web/themes/WVBaseTheme/src/js/script.js
+++ portfolio2020/web/themes/WVBaseTheme/src/js/script.js
@@ -90,12 +90,12 @@ jQuery(document).ready( function($) {
         var content_slice1 = first_line('textcontent');
         console.log(content_slice1);
         $(".title-first-line").remove();
-        if (matchMedia('(max-width: 342px)').matches) {
-            var svgElement = "<svg class='title-first-line' xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'><text class='h1' id='lift' x='50%' y='14%'>" + content_slice1 + "</text><use id='use' xlink:href='#lift' /></svg>";
+        if (matchMedia('(max-width: 480px)').matches) {
+            var svgElement = "<svg class='title-first-line' xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'><text class='h1' id='lift' x='50%' y='25' text-anchor='middle'>" + content_slice1 + "</text><use id='use' xlink:href='#lift' /></svg>";
         } else if (matchMedia('(max-width: 1199px)').matches) {
-            var svgElement = "<svg class='title-first-line' xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'><text class='h1' id='lift' x='50%' y='18%'>" + content_slice1 + "</text><use id='use' xlink:href='#lift' /></svg>";
+            var svgElement = "<svg class='title-first-line' xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'><text class='h1' id='lift' x='50%' y='38' text-anchor='middle'>" + content_slice1 + "</text><use id='use' xlink:href='#lift' /></svg>";
         } else {
-            var svgElement = "<svg class='title-first-line' xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'><text class='h1' id='lift' x='50%' y='13%'>" + content_slice1 + "</text><use id='use' xlink:href='#lift' /></svg>";
+            var svgElement = "<svg class='title-first-line' xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'><text class='h1' id='lift' x='50%' y='86' text-anchor='middle'>" + content_slice1 + "</text><use id='use' xlink:href='#lift' /></svg>";
         }
         $(".node-title-wrapper").append(svgElement);
     }
