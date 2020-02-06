f02e126f5ab2fb64e92ef23fd3a4500a2ba0c014
Wim Verpoorten <wim.verpoorten@hotmail.com>
2020-02-06 10:41:09 +0100

    work teaser title fix different approach: applied to all teasers

--- portfolio2020/web/themes/WVBaseTheme/src/js/script.js
+++ portfolio2020/web/themes/WVBaseTheme/src/js/script.js
@@ -125,7 +125,6 @@ jQuery(document).ready( function($) {
         var strokeTextTeaser1 = new StrokeText('work-teaser-1', options);
         clearTimeout(resizeTimeoutTeaser1);
         resizeTimeoutTeaser1 = setTimeout(function() {
-            // reset and re-init so strokeText.js can re-evaluate container size
             strokeTextTeaser1.reset();
             strokeTextTeaser1 = new StrokeText('work-teaser-1', options);
             applyStrokeTeaser(strokeTextTeaser1);
@@ -133,17 +132,267 @@ jQuery(document).ready( function($) {
         }, 100);
     }
     function handleViewportChangeTeaser1() {
-        // timeout to debounce
         clearTimeout(resizeTimeoutTeaser1);
         resizeTimeoutTeaser1 = setTimeout(function() {
-            // reset and re-init so strokeText.js can re-evaluate container size
             strokeTextTeaser1.reset();
             strokeTextTeaser1 = new StrokeText('work-teaser-1', options);
             applyStrokeTeaser(strokeTextTeaser1);
+        }, 100);
+    }
+    /* end of teaser1 */
+
+    /* teaser2 */
+    if( $("#work-teaser-2").length !== 0 ) {
+        console.log("teaser2");
+        var resizeTimeoutTeaser2;
+        var strokeTextTeaser2 = new StrokeText('work-teaser-2', options);
+        clearTimeout(resizeTimeoutTeaser2);
+        resizeTimeoutTeaser2 = setTimeout(function() {
+            strokeTextTeaser2.reset();
+            strokeTextTeaser2 = new StrokeText('work-teaser-2', options);
+            applyStrokeTeaser(strokeTextTeaser2);
 
         }, 100);
     }
-    /* end of teaser 1 */
+    function handleViewportChangeTeaser2() {
+        clearTimeout(resizeTimeoutTeaser2);
+        resizeTimeoutTeaser2 = setTimeout(function() {
+            strokeTextTeaser2.reset();
+            strokeTextTeaser2 = new StrokeText('work-teaser-2', options);
+            applyStrokeTeaser(strokeTextTeaser2);
+        }, 100);
+    }
+    /* end of teaser2 */
+
+    /* teaser3 */
+    if( $("#work-teaser-3").length !== 0 ) {
+        console.log("teaser3");
+        var resizeTimeoutTeaser3;
+        var strokeTextTeaser3 = new StrokeText('work-teaser-3', options);
+        clearTimeout(resizeTimeoutTeaser3);
+        resizeTimeoutTeaser3 = setTimeout(function() {
+            strokeTextTeaser3.reset();
+            strokeTextTeaser3 = new StrokeText('work-teaser-3', options);
+            applyStrokeTeaser(strokeTextTeaser3);
+
+        }, 100);
+    }
+    function handleViewportChangeTeaser3() {
+        clearTimeout(resizeTimeoutTeaser3);
+        resizeTimeoutTeaser3 = setTimeout(function() {
+            strokeTextTeaser3.reset();
+            strokeTextTeaser3 = new StrokeText('work-teaser-3', options);
+            applyStrokeTeaser(strokeTextTeaser3);
+        }, 100);
+    }
+    /* end of teaser3 */
+
+    /* teaser4 */
+    if( $("#work-teaser-4").length !== 0 ) {
+        console.log("teaser4");
+        var resizeTimeoutTeaser4;
+        var strokeTextTeaser4 = new StrokeText('work-teaser-4', options);
+        clearTimeout(resizeTimeoutTeaser4);
+        resizeTimeoutTeaser4 = setTimeout(function() {
+            strokeTextTeaser4.reset();
+            strokeTextTeaser4 = new StrokeText('work-teaser-4', options);
+            applyStrokeTeaser(strokeTextTeaser4);
+
+        }, 100);
+    }
+    function handleViewportChangeTeaser4() {
+        clearTimeout(resizeTimeoutTeaser4);
+        resizeTimeoutTeaser4 = setTimeout(function() {
+            strokeTextTeaser4.reset();
+            strokeTextTeaser4 = new StrokeText('work-teaser-4', options);
+            applyStrokeTeaser(strokeTextTeaser4);
+        }, 100);
+    }
+    /* end of teaser4 */
+
+    /* teaser5 */
+    if( $("#work-teaser-5").length !== 0 ) {
+        console.log("teaser5");
+        var resizeTimeoutTeaser5;
+        var strokeTextTeaser5 = new StrokeText('work-teaser-5', options);
+        clearTimeout(resizeTimeoutTeaser5);
+        resizeTimeoutTeaser5 = setTimeout(function() {
+            strokeTextTeaser5.reset();
+            strokeTextTeaser5 = new StrokeText('work-teaser-5', options);
+            applyStrokeTeaser(strokeTextTeaser5);
+
+        }, 100);
+    }
+    function handleViewportChangeTeaser5() {
+        clearTimeout(resizeTimeoutTeaser5);
+        resizeTimeoutTeaser5 = setTimeout(function() {
+            strokeTextTeaser5.reset();
+            strokeTextTeaser5 = new StrokeText('work-teaser-5', options);
+            applyStrokeTeaser(strokeTextTeaser5);
+        }, 100);
+    }
+    /* end of teaser5 */
+
+    /* teaser6 */
+    if( $("#work-teaser-6").length !== 0 ) {
+        console.log("teaser6");
+        var resizeTimeoutTeaser6;
+        var strokeTextTeaser6 = new StrokeText('work-teaser-6', options);
+        clearTimeout(resizeTimeoutTeaser6);
+        resizeTimeoutTeaser6 = setTimeout(function() {
+            strokeTextTeaser6.reset();
+            strokeTextTeaser6 = new StrokeText('work-teaser-6', options);
+            applyStrokeTeaser(strokeTextTeaser6);
+
+        }, 100);
+    }
+    function handleViewportChangeTeaser6() {
+        clearTimeout(resizeTimeoutTeaser6);
+        resizeTimeoutTeaser6 = setTimeout(function() {
+            strokeTextTeaser6.reset();
+            strokeTextTeaser6 = new StrokeText('work-teaser-6', options);
+            applyStrokeTeaser(strokeTextTeaser6);
+        }, 100);
+    }
+    /* end of teaser6 */
+
+    /* teaser7 */
+    if( $("#work-teaser-7").length !== 0 ) {
+        console.log("teaser7");
+        var resizeTimeoutTeaser7;
+        var strokeTextTeaser7 = new StrokeText('work-teaser-7', options);
+        clearTimeout(resizeTimeoutTeaser7);
+        resizeTimeoutTeaser7 = setTimeout(function() {
+            strokeTextTeaser7.reset();
+            strokeTextTeaser7 = new StrokeText('work-teaser-7', options);
+            applyStrokeTeaser(strokeTextTeaser7);
+
+        }, 100);
+    }
+    function handleViewportChangeTeaser7() {
+        clearTimeout(resizeTimeoutTeaser7);
+        resizeTimeoutTeaser7 = setTimeout(function() {
+            strokeTextTeaser7.reset();
+            strokeTextTeaser7 = new StrokeText('work-teaser-7', options);
+            applyStrokeTeaser(strokeTextTeaser7);
+        }, 100);
+    }
+    /* end of teaser7 */
+
+    /* teaser8 */
+    if( $("#work-teaser-8").length !== 0 ) {
+        console.log("teaser8");
+        var resizeTimeoutTeaser8;
+        var strokeTextTeaser8 = new StrokeText('work-teaser-8', options);
+        clearTimeout(resizeTimeoutTeaser8);
+        resizeTimeoutTeaser8 = setTimeout(function() {
+            strokeTextTeaser8.reset();
+            strokeTextTeaser8 = new StrokeText('work-teaser-8', options);
+            applyStrokeTeaser(strokeTextTeaser8);
+
+        }, 100);
+    }
+    function handleViewportChangeTeaser8() {
+        clearTimeout(resizeTimeoutTeaser8);
+        resizeTimeoutTeaser8 = setTimeout(function() {
+            strokeTextTeaser8.reset();
+            strokeTextTeaser8 = new StrokeText('work-teaser-8', options);
+            applyStrokeTeaser(strokeTextTeaser8);
+        }, 100);
+    }
+    /* end of teaser8 */
+
+    /* teaser9 */
+    if( $("#work-teaser-9").length !== 0 ) {
+        console.log("teaser9");
+        var resizeTimeoutTeaser9;
+        var strokeTextTeaser9 = new StrokeText('work-teaser-9', options);
+        clearTimeout(resizeTimeoutTeaser9);
+        resizeTimeoutTeaser9 = setTimeout(function() {
+            strokeTextTeaser9.reset();
+            strokeTextTeaser9 = new StrokeText('work-teaser-9', options);
+            applyStrokeTeaser(strokeTextTeaser9);
+
+        }, 100);
+    }
+    function handleViewportChangeTeaser9() {
+        clearTimeout(resizeTimeoutTeaser9);
+        resizeTimeoutTeaser9 = setTimeout(function() {
+            strokeTextTeaser9.reset();
+            strokeTextTeaser9 = new StrokeText('work-teaser-9', options);
+            applyStrokeTeaser(strokeTextTeaser9);
+        }, 100);
+    }
+    /* end of teaser9 */
+
+    /* teaser10 */
+    if( $("#work-teaser-10").length !== 0 ) {
+        console.log("teaser10");
+        var resizeTimeoutTeaser10;
+        var strokeTextTeaser10 = new StrokeText('work-teaser-10', options);
+        clearTimeout(resizeTimeoutTeaser10);
+        resizeTimeoutTeaser10 = setTimeout(function() {
+            strokeTextTeaser10.reset();
+            strokeTextTeaser10 = new StrokeText('work-teaser-10', options);
+            applyStrokeTeaser(strokeTextTeaser10);
+
+        }, 100);
+    }
+    function handleViewportChangeTeaser10() {
+        clearTimeout(resizeTimeoutTeaser10);
+        resizeTimeoutTeaser10 = setTimeout(function() {
+            strokeTextTeaser10.reset();
+            strokeTextTeaser10 = new StrokeText('work-teaser-10', options);
+            applyStrokeTeaser(strokeTextTeaser10);
+        }, 100);
+    }
+    /* end of teaser10 */
+
+    /* teaser11 */
+    if( $("#work-teaser-11").length !== 0 ) {
+        console.log("teaser11");
+        var resizeTimeoutTeaser11;
+        var strokeTextTeaser11 = new StrokeText('work-teaser-11', options);
+        clearTimeout(resizeTimeoutTeaser11);
+        resizeTimeoutTeaser11 = setTimeout(function() {
+            strokeTextTeaser11.reset();
+            strokeTextTeaser11 = new StrokeText('work-teaser-11', options);
+            applyStrokeTeaser(strokeTextTeaser11);
+
+        }, 100);
+    }
+    function handleViewportChangeTeaser11() {
+        clearTimeout(resizeTimeoutTeaser11);
+        resizeTimeoutTeaser11 = setTimeout(function() {
+            strokeTextTeaser11.reset();
+            strokeTextTeaser11 = new StrokeText('work-teaser-11', options);
+            applyStrokeTeaser(strokeTextTeaser11);
+        }, 100);
+    }
+    /* end of teaser11 */
+    
+    /* teaser12 */
+    if( $("#work-teaser-12").length !== 0 ) {
+        console.log("teaser12");
+        var resizeTimeoutTeaser12;
+        var strokeTextTeaser12 = new StrokeText('work-teaser-12', options);
+        clearTimeout(resizeTimeoutTeaser12);
+        resizeTimeoutTeaser12 = setTimeout(function() {
+            strokeTextTeaser12.reset();
+            strokeTextTeaser12 = new StrokeText('work-teaser-12', options);
+            applyStrokeTeaser(strokeTextTeaser12);
+
+        }, 100);
+    }
+    function handleViewportChangeTeaser12() {
+        clearTimeout(resizeTimeoutTeaser12);
+        resizeTimeoutTeaser12 = setTimeout(function() {
+            strokeTextTeaser12.reset();
+            strokeTextTeaser12 = new StrokeText('work-teaser-12', options);
+            applyStrokeTeaser(strokeTextTeaser12);
+        }, 100);
+    }
+    /* end of teaser12 */
 
 
     /***************************
@@ -157,7 +406,18 @@ jQuery(document).ready( function($) {
 
     var resizeHandler = function(){
         handleViewportChange();
-        handleViewportChangeTeaser1();
+        $("#work-teaser-1").length !== 0 ? handleViewportChangeTeaser1() : null;
+        $("#work-teaser-2").length !== 0 ? handleViewportChangeTeaser2() : null;
+        $("#work-teaser-3").length !== 0 ? handleViewportChangeTeaser3() : null;
+        $("#work-teaser-4").length !== 0 ? handleViewportChangeTeaser4() : null;
+        $("#work-teaser-5").length !== 0 ? handleViewportChangeTeaser5() : null;
+        $("#work-teaser-6").length !== 0 ? handleViewportChangeTeaser6() : null;
+        $("#work-teaser-7").length !== 0 ? handleViewportChangeTeaser7() : null;
+        $("#work-teaser-8").length !== 0 ? handleViewportChangeTeaser8() : null;
+        $("#work-teaser-9").length !== 0 ? handleViewportChangeTeaser9() : null;
+        $("#work-teaser-10").length !== 0 ? handleViewportChangeTeaser10() : null;
+        $("#work-teaser-11").length !== 0 ? handleViewportChangeTeaser11() : null;
+        $("#work-teaser-12").length !== 0 ? handleViewportChangeTeaser12() : null;
     };
 
     $(window).resize(resizeHandler);
